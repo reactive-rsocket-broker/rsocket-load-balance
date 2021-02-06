7c5c8afa2e6181ab8c6b522c200c992cd3321e5e
linux_china <libing.chen@gmail.com>
2021-02-06 15:55:44 -0800

    RSocketLoadBalanceEndpoint

---
+++ rsocket-loadbalance-spring-boot-starter/src/main/java/com/alibaba/rsocket/loadbalance/RSocketLoadBalanceEndpoint.java
@@ -0,0 +1,30 @@
+package com.alibaba.rsocket.loadbalance;
+
+import org.springframework.boot.actuate.endpoint.annotation.Endpoint;
+import org.springframework.boot.actuate.endpoint.annotation.ReadOperation;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+@Endpoint(id = "rsocketlb")
+public class RSocketLoadBalanceEndpoint {
+    private final RSocketServiceRegistry rsocketServiceRegistry;
+
+    public RSocketLoadBalanceEndpoint(RSocketServiceRegistry rsocketServiceRegistry) {
+        this.rsocketServiceRegistry = rsocketServiceRegistry;
+    }
+
+    @ReadOperation
+    public Map<String, Object> info() {
+        Map<String, Object> info = new HashMap<>();
+        Map<String, List<String>> services = new HashMap<>();
+        for (Map.Entry<String, List<RSocketServerInstance>> entry : rsocketServiceRegistry.getSnapshots().entrySet()) {
+            services.put(entry.getKey(), entry.getValue().stream().map(RSocketServerInstance::getURI).collect(Collectors.toList()));
+        }
+        info.put("services", services);
+        info.put("lastRefreshAt", rsocketServiceRegistry.getLastRefreshTimestamp());
+        return info;
+    }
+}
