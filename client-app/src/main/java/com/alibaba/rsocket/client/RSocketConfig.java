d745e7a93eb98f5b99fc3cfa04147b3798a1f264
linux_china <libing.chen@gmail.com>
2021-02-06 15:51:41 -0800

    RSocketConfig

---
+++ client-app/src/main/java/com/alibaba/rsocket/client/RSocketConfig.java
@@ -0,0 +1,55 @@
+package com.alibaba.rsocket.client;
+
+import com.alibaba.calculator.ExchangeCalculatorService;
+import com.alibaba.calculator.MathCalculatorService;
+import com.alibaba.rsocket.loadbalance.RSocketServiceRegistry;
+import com.alibaba.rsocket.loadbalance.proxy.RSocketRemoteServiceBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.codec.cbor.Jackson2CborDecoder;
+import org.springframework.http.codec.cbor.Jackson2CborEncoder;
+import org.springframework.messaging.rsocket.RSocketRequester;
+import org.springframework.messaging.rsocket.RSocketStrategies;
+
+@Configuration
+public class RSocketConfig {
+
+    @Bean
+    public RSocketStrategies rSocketStrategies() {
+        return RSocketStrategies.builder()
+                .encoders(encoders -> encoders.add(new Jackson2CborEncoder()))
+                .decoders(decoders -> decoders.add(new Jackson2CborDecoder()))
+                .build();
+    }
+
+    @Bean
+    public RSocketRequester mathCalculatorRequester(RSocketRequester.Builder builder,
+                                                    RSocketServiceRegistry rsocketServiceRegistry) {
+        return rsocketServiceRegistry.buildLoadBalanceRSocket(MathCalculatorService.RSOCKET_SERVICE_NAME, builder);
+    }
+
+    @Bean
+    public RSocketRequester exchangeCalculatorRequester(RSocketRequester.Builder builder,
+                                                        RSocketServiceRegistry rsocketServiceRegistry) {
+        return rsocketServiceRegistry.buildLoadBalanceRSocket(ExchangeCalculatorService.RSOCKET_SERVICE_NAME, builder);
+    }
+
+    @Bean
+    public MathCalculatorService mathCalculatorService(RSocketRequester mathCalculatorRequester) {
+        RSocketRemoteServiceBuilder<MathCalculatorService> builder = new RSocketRemoteServiceBuilder<>();
+        return builder.serviceName(MathCalculatorService.RSOCKET_SERVICE_NAME)
+                .serviceInterface(MathCalculatorService.class)
+                .rsocketRequester(mathCalculatorRequester)
+                .build();
+    }
+
+    @Bean
+    public ExchangeCalculatorService exchangeCalculatorService(RSocketRequester exchangeCalculatorRequester) {
+        RSocketRemoteServiceBuilder<ExchangeCalculatorService> builder = new RSocketRemoteServiceBuilder<>();
+        return builder.serviceName(ExchangeCalculatorService.RSOCKET_SERVICE_NAME)
+                .serviceInterface(ExchangeCalculatorService.class)
+                .rsocketRequester(exchangeCalculatorRequester)
+                .build();
+    }
+
+}
