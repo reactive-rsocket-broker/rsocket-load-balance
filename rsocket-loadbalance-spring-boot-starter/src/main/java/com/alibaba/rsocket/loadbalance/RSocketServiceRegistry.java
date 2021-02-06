c3970a0a8e55aca46559a87227e4056c785399cd
linux_china <libing.chen@gmail.com>
2021-02-06 15:53:49 -0800

    RSocketServiceRegistry

---
+++ rsocket-loadbalance-spring-boot-starter/src/main/java/com/alibaba/rsocket/loadbalance/RSocketServiceRegistry.java
@@ -0,0 +1,20 @@
+package com.alibaba.rsocket.loadbalance;
+
+import io.rsocket.loadbalance.LoadbalanceTarget;
+import org.springframework.messaging.rsocket.RSocketRequester;
+import reactor.core.publisher.Flux;
+
+import java.util.Date;
+import java.util.List;
+import java.util.Map;
+
+public interface RSocketServiceRegistry {
+
+    Flux<List<LoadbalanceTarget>> getServers(String serviceName);
+
+    RSocketRequester buildLoadBalanceRSocket(String serviceName, RSocketRequester.Builder builder);
+
+    Map<String, List<RSocketServerInstance>> getSnapshots();
+
+    Date getLastRefreshTimestamp();
+}
