63bdc7bda6f9ae851dc602ed652c9a6d9302e006
linux_china <libing.chen@gmail.com>
2021-02-06 15:55:57 -0800

    RSocketLoadBalanceConfiguration

---
+++ rsocket-loadbalance-spring-boot-starter/src/main/java/com/alibaba/rsocket/loadbalance/RSocketLoadBalanceConfiguration.java
@@ -0,0 +1,21 @@
+package com.alibaba.rsocket.loadbalance;
+
+import org.springframework.cloud.client.discovery.ReactiveDiscoveryClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+@Configuration
+@EnableScheduling
+public class RSocketLoadBalanceConfiguration {
+
+    @Bean
+    public RSocketServiceDiscoveryRegistry rsocketServiceDiscoveryRegistry(ReactiveDiscoveryClient discoveryClient) {
+        return new RSocketServiceDiscoveryRegistry(discoveryClient);
+    }
+
+    @Bean
+    public RSocketLoadBalanceEndpoint rsocketLoadBalanceEndpoint(RSocketServiceRegistry rsocketServiceRegistry) {
+        return new RSocketLoadBalanceEndpoint(rsocketServiceRegistry);
+    }
+}
