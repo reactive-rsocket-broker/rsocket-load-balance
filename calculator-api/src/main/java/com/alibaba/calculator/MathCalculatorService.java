f98cd6bb3b48af10f57bcfab0e8fa7f85d02888e
linux_china <libing.chen@gmail.com>
2021-02-06 15:38:38 -0800

    fix service full name

--- calculator-api/src/main/java/com/alibaba/calculator/MathCalculatorService.java
+++ calculator-api/src/main/java/com/alibaba/calculator/MathCalculatorService.java
@@ -3,7 +3,7 @@ package com.alibaba.calculator;
 import reactor.core.publisher.Mono;
 
 public interface MathCalculatorService {
-    String RSOCKET_SERVICE_NAME = "com.example.calculator.MathCalculatorService";
+    String RSOCKET_SERVICE_NAME = "com.alibaba.calculator.MathCalculatorService";
 
     Mono<Integer> square(Integer input);
 }
