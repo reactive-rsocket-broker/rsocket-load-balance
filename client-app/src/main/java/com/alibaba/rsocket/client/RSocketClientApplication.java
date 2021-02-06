9cfd6a97c0a1b0322e4b9be13315f47ed69af19d
linux_china <libing.chen@gmail.com>
2021-02-06 15:51:49 -0800

    RSocketClientApplication

---
+++ client-app/src/main/java/com/alibaba/rsocket/client/RSocketClientApplication.java
@@ -0,0 +1,15 @@
+package com.alibaba.rsocket.client;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+public class RSocketClientApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RSocketClientApplication.class, args);
+    }
+
+}
