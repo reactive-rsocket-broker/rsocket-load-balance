6edb67ae4d483f731cb041ffc2b9772b27bcb7bf
linux_china <libing.chen@gmail.com>
2021-02-06 15:38:13 -0800

    ExchangeCalculatorService

---
+++ calculator-api/src/main/java/com/alibaba/calculator/ExchangeCalculatorService.java
@@ -0,0 +1,14 @@
+package com.alibaba.calculator;
+
+import reactor.core.publisher.Mono;
+
+public interface ExchangeCalculatorService {
+    String RSOCKET_SERVICE_NAME = "com.alibaba.calculator.ExchangeCalculatorService";
+
+    Mono<Double> exchange(ExchangeRequest request);
+
+    default Mono<Double> dollarToRMB(double amount) {
+        return exchange(new ExchangeRequest(amount, "USD", "CNY"));
+    }
+
+}
