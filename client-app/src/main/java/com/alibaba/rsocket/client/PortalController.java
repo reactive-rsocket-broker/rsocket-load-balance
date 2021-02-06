6540c2d5278ddecd46bd2492eb593eee6ab4e3a1
linux_china <libing.chen@gmail.com>
2021-02-06 15:52:06 -0800

    PortalController

---
+++ client-app/src/main/java/com/alibaba/rsocket/client/PortalController.java
@@ -0,0 +1,40 @@
+package com.alibaba.rsocket.client;
+
+import com.alibaba.calculator.ExchangeCalculatorService;
+import com.alibaba.calculator.MathCalculatorService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.messaging.rsocket.RSocketRequester;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Mono;
+
+@RestController
+public class PortalController {
+    @Autowired
+    private RSocketRequester mathCalculatorRequester;
+    @Autowired
+    private MathCalculatorService mathCalculatorService;
+    @Autowired
+    private ExchangeCalculatorService exchangeCalculatorService;
+
+    @GetMapping("/square/{number}")
+    public Mono<String> square(@PathVariable("number") int number) {
+        return mathCalculatorService.square(number)
+                .map(result -> number + "*" + number + "=" + result);
+    }
+
+    @GetMapping("/square2/{number}")
+    public Mono<String> square2(@PathVariable("number") int number) {
+        return mathCalculatorRequester.route("com.example.calculator.MathCalculatorService.square")
+                .data(number)
+                .retrieveMono(Integer.class)
+                .map(result -> number + "*" + number + "=" + result);
+    }
+
+    @GetMapping("/exchange/{number}")
+    public Mono<String> exchangeUSD2CNY(@PathVariable("number") int number) {
+        return exchangeCalculatorService.dollarToRMB(number)
+                .map(result -> number + "$ = " + result + "RMB");
+    }
+}
